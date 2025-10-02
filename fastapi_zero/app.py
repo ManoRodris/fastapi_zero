ef5ea58e127e390a9bc25da15d47b157f20f3250
Rodrigo <rodrigodoreafelix@gmail.com>
2025-10-01 23:34:34 -0300

    Iniciando CRUD na aplicação

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,19 +1,35 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-
+from fastapi_zero.schemas import (
+    UserSchema, UserPublic, UserDB
+)
+from http import HTTPStatus
 app = FastAPI()
+
+database = []
 
 
 # @app.get('/')
 # def read_root():
 #     return {'message': 'Olá Mundo'}
 
-@app.get('/', response_class=HTMLResponse)
-def ola_mundo():
-    return '''
-    <html>
-    <head>
-        <title>Olá Mundo</title>
-        <h1>Olá Mundo</h1>
-    </head>
-    </html>'''
+# @app.get('/', response_class=HTMLResponse)
+# def ola_mundo():
+#     return '''
+#     <html>
+#     <head>
+#         <title>Olá Mundo</title>
+#         <h1>Olá Mundo</h1>
+#     </head>
+#     </html>'''
+
+@app.post('/user/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def creating_user(user : UserSchema):
+    user_with_id = UserDB (
+        id = len(database) + 1,
+        **user.model_dump()
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
